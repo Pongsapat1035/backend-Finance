35cff3c7a8c921163ed3dcb00f97ade9baaf65f0
pongsapat <pongsapat.c.work@gmail.com>
2026-04-18 17:17:38 +0700

    feat: add jwt strategy

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'my_super_secret_fallback_key',
+    });
+  }
+
+  async validate(payload: { userId: number }) {
+    return { userId: payload.userId };
+  }
+}
